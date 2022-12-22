f25c928ae709507512dc0eb59fa4c0d412cdeee0
Sw4rtz <4aronswartz@gmail.com>
2022-12-22 16:58:03 +0000

    Init Créditos
    Changelog:
    - Adicionado parte da view créditos

--- app/src/main/java/pt/ipt/dam2022/devicemanipulator/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam2022/devicemanipulator/MainActivity.kt
@@ -1,11 +1,17 @@
 package pt.ipt.dam2022.devicemanipulator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        val buttonClick = findViewById<Button>(R.id.creditos)
+        buttonClick.setOnClickListener {
+                setContentView(R.layout.creditos)
+            }
+        }
 }
