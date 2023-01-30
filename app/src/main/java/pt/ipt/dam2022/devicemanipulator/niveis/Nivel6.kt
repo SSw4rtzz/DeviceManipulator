c66fe1fa2c04db898c3b489221daa53124376d04
Sw4rtz <4aronswartz@gmail.com>
2023-01-30 19:28:35 +0000

    Nivel 6
    Changelog:
    - Nível 6 adicionado à aplicação
    - Este consiste em colocar o modo avião no telemóvel para ligar as luzes da pista e desbloquear a movimentação do avião pelo utilizador, depois o utilizador só tem de o lançar com o dedo para a frente para o nível ser concluído

--- app/src/main/java/pt/ipt/dam2022/devicemanipulator/niveis/Nivel6.kt
+++ app/src/main/java/pt/ipt/dam2022/devicemanipulator/niveis/Nivel6.kt
@@ -1,13 +1,10 @@
 package pt.ipt.dam2022.devicemanipulator.niveis
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.graphics.Color
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -20,7 +17,6 @@ import android.view.animation.AnimationUtils
 import android.view.animation.TranslateAnimation
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import pt.ipt.dam2022.devicemanipulator.R
 
@@ -28,11 +24,13 @@ class Nivel6 : AppCompatActivity() {
 
     private lateinit var pisca: Animation
     private lateinit var aviao : ImageView
+    private lateinit var btnProximoNivel : Button
+
 
     var nivelConcluido = false
 
     private var nivelAtual = 6
-    private var stringDica = "Imagina-te num avião, segue as regras"
+    private var stringDica = "Segue as regras de segurança de um passageiro e depois dá uma ajudinha na descolagem"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +39,7 @@ class Nivel6 : AppCompatActivity() {
         val layout = findViewById<View>(R.id.layoutNivel6)
         val btnDica = findViewById<ImageView>(R.id.dica)
 
+        // Cria um Snackbar com a mensagem stringDica e tempo de duração de 5000 milissegundos
         val dica = Snackbar.make(layout, stringDica, 5000)
         //Botão para mostrar uma dica
         btnDica.setOnClickListener {
@@ -48,7 +47,7 @@ class Nivel6 : AppCompatActivity() {
         }
         // ************** BOTÃO PROXIMO NIVEL **************
         //Evento onClick do botão "Próximo Nivel" levando a aplicação à activity do próximo nivel
-        var btnProximoNivel = findViewById<Button>(R.id.btnProximoNivel)
+        btnProximoNivel = findViewById(R.id.btnProximoNivel)
         btnProximoNivel.setOnClickListener {
             val intent = Intent(this, Nivel6::class.java)
             startActivity(intent)
@@ -59,6 +58,7 @@ class Nivel6 : AppCompatActivity() {
         //************** BOTÃO PROXIMO NIVEL **************
 
         //*************** INICIO GUARDA NIVEL ****************
+        // Salva o nível atual em shared preferences
         val sharedPref = getSharedPreferences("game_data", Context.MODE_PRIVATE)
         val editor = sharedPref.edit()
         editor.putInt("nivel_atual", nivelAtual)
@@ -66,7 +66,7 @@ class Nivel6 : AppCompatActivity() {
         Log.d("Debug", "Save Criado $nivelAtual")
         //**************** FIM GUARDA NIVEL ****************
 
-
+        // Carrega a animação "pisca"
         pisca = AnimationUtils.loadAnimation(this, R.anim.animluzes)
     }
 
@@ -74,15 +74,18 @@ class Nivel6 : AppCompatActivity() {
 
     private val receiver = object : BroadcastReceiver(){
         override fun onReceive(context: Context, intent: Intent) {
+            // Recebe o estado atual do modo avião
             val modoAviaoON = intent.getBooleanExtra("state",false)
             if (modoAviaoON){
                 Log.d("Debug", "Modo voo ativado")
+
+                // Inicia uma thread separada para manipular os animations
                 val handlerThread = HandlerThread("thread1")
                 handlerThread.start()
                 val handler = Handler(handlerThread.looper)
                 handler.postDelayed({
 
-                    //"Acende as luzes da pista" Coloca os circulos ld e le a verde gradualmente e 2 segundos após a ativação do modo avião
+                    //"Acende as luzes da pista" gradualmente e 2 segundos após a ativação do modo avião
                     for (i in 1..5) {
                         val idDir = "luzDir$i"
                         val ld = findViewById<ImageView>(
@@ -101,13 +104,14 @@ class Nivel6 : AppCompatActivity() {
                             )
                         )
                         handler.postDelayed({
+                            // Define a cor verde gradualmente para cada luz da pista
                             ld.setColorFilter(Color.parseColor("#0aae00"))
                             le.setColorFilter(Color.parseColor("#0aae00"))
                         }, (i - 1L) * 800)
                     }
                 }, 2000)
 
-                //Coloca as luzes da pista a piscar
+                // Coloca as luzes da pista a piscar após 5.5 segundos de ativação do modo avião
                 handler.postDelayed({
                 for (i in 1..5) {
                     val idDir = "luzDir$i"
@@ -131,44 +135,70 @@ class Nivel6 : AppCompatActivity() {
                 }
                 }, 5500)
 
-                //Altera a dica para o segundo passo do nível
-                stringDica = "Força o avião a levantar voo"
                 aviao = findViewById(R.id.aviao)
+                var animationStarted = false
+
+                // Move o avião através de um listener que lê o toque no ecrã
                 aviao.setOnTouchListener { view, event ->
                     when (event.action) {
-                        // FALTA FAZER BLOQUEAR AÇÕES DO ACTION_MOVE QUANDO O IF FOR EXECUTADO
                         MotionEvent.ACTION_MOVE -> {
-                            view.animate().y(event.rawY - view.height/2).setDuration(0).start()
-                            val newY = event.rawY - view.height/2
-                            if (newY < 500) {
-                                Log.d("Debug", "Nivel passado")
-                                nivelConcluido = true
-                            val descolagem = TranslateAnimation(0f, 0f, view.y-300, view.y - view.height*4)
-                            descolagem.duration = 1000
-                            descolagem.fillAfter = true
-                            view.startAnimation(descolagem)}
+                            // Verifica se a animação já começou, se ainda não tiver começado, permite que o utilizador continue a mover o aviao
+                            if (!animationStarted) {
+                                // Move o avião para o local tocado pelo usuário
+                                view.animate().y(event.rawY - view.height / 2).setDuration(0).start()
+                                // Obtém a nova posição Y do avião
+                                val newY = event.rawY - view.height / 2
+                                // Verifica se a nova posição Y é menor que 300, se for conclui o nível
+                                if (newY < 300) {
+                                    Log.d("Debug", "Nivel passado")
+                                    // Define a variável nivelConcluido como verdadeira
+                                    nivelConcluido = true
+                                    // Cria uma animação de descolagem para o avião
+                                    val descolagem = TranslateAnimation(
+                                        0f,
+                                        0f,
+                                        view.y - 300,
+                                        view.y - view.height * 4
+                                    )
+                                    animationStarted = true
+                                    // Define a duração da animação como 1 segundo
+                                    descolagem.duration = 1000
+                                    // Mantém o avião na posição final após a animação
+                                    descolagem.fillAfter = true
+                                    // Inicia a animação de descolagem do avião
+                                    view.startAnimation(descolagem)
+                                }
+                            }
                         }
+                        // Quando o utilizador para de tocar no ecrã
                         MotionEvent.ACTION_UP -> {
-
+                            // Define a variável animationStarted como falsa
+                            animationStarted = false
+                            // Verifica se o nível foi concluído, se tiver sido, coloca o botão "Próximo Nivel" visivel
+                            if(nivelConcluido) {
+                                btnProximoNivel.visibility = View.VISIBLE
+                            }
                         }
                     }
                     true
                 }
-
-
-
             } else{
                 Log.d("Debug", "Modo voo desativado")
-                //Volta a colocar a dica na primeira fase do nível
-                stringDica = "Imagina-te num avião, segue as regras"
 
+                // Loop de 1 a 5 para alterar as propriedades de 10 imagens
                 for(i in 1..5 ){
+                    // Construção da identificação da circulo "luzDir" com o número atual do loop
                     val idDir = "luzDir$i"
                     val ld = findViewById<ImageView>(resources.getIdentifier(idDir, "id", packageName))
+                    // Construção da identificação do circulo "luzEsq" com o número atual do loop
                     val idEsq = "luzEsq$i"
                     val le = findViewById<ImageView>(resources.getIdentifier(idEsq, "id", packageName))
+
+                    // Alteração da cor das luzes da pista para verde
                         ld.setColorFilter(Color.parseColor("#242424"))
                         le.setColorFilter(Color.parseColor("#242424"))
+
+                    // Limpeza das animações das imagens
                     ld.clearAnimation()
                     le.clearAnimation()
                 }
@@ -179,11 +209,13 @@ class Nivel6 : AppCompatActivity() {
     override fun onResume(){
         super.onResume()
         val filter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        //Começa o listener do receiver
         registerReceiver(receiver,filter)
     }
 
     override fun onPause() {
         super.onPause()
+        //Para o listener do receiver
         unregisterReceiver(receiver)
     }
 }
